85477c59a0802d9ab76d468c678f50234d72111b
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2026-02-17 17:41:48 +0900

    Changes to be committed: 메인 파일 ts로 변경, assets 경로 수정, index.html 수정 	modified:   index.html 	new file:   package-lock.json 	modified:   package.json 	modified:   src/js/assets_path.js 	renamed:    src/js/main.js -> src/main.ts 	new file:   src/vite-env.d.ts

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,5 @@
-/**
- * This is the main script which executes the game.
- * General explanations for the all source code files of the game are following.
- *
- ********************************************************************************************************************
- * This web version of the Pikachu Volleyball is made by
- * reverse engineering the core part of the original Pikachu Volleyball game
- * which is developed by "1997 (C) SACHI SOFT / SAWAYAKAN Programmers" & "1997 (C) Satoshi Takenouchi".
- *
- * "physics.js", "cloud_and_wave.js", and some codes in "view.js" are the results of this reverse engineering.
- * Refer to the comments in each file for the machine code addresses of the original functions.
- ********************************************************************************************************************
- *
- * This web version game is mainly composed of three parts which follows MVC pattern.
- *  1) "physics.js" (Model): The physics engine which takes charge of the dynamics of the ball and the players (Pikachus).
- *                           It is gained by reverse engineering the machine code of the original game.
- *  2) "view.js" (View): The rendering part of the game which depends on pixi.js (https://www.pixijs.com/, https://github.com/pixijs/pixi.js) library.
- *                       Some codes in this part is gained by reverse engineering the original machine code.
- *  3) "pikavolley.js" (Controller): Make the game work by controlling the Model and the View according to the user input.
- *
- * And explanations for other source files are below.
- *  - "cloud_and_wave.js": This is also a Model part which takes charge of the clouds and wave motion in the game. Of course, it is also rendered by "view.js".
- *                         It is also gained by reverse engineering the original machine code.
- *  - "keyboard.js": Support the Controller("pikavolley.js") to get a user input via keyboard.
- *  - "audio.js": The game audio or sounds. It depends on pixi-sound (https://github.com/pixijs/pixi-sound) library.
- *  - "rand.js": For the random function used in the Models ("physics.js", "cloud_and_wave.js").
- *  - "assets_path.js": For the assets (image files, sound files) locations.
- *  - "ui.js": For the user interface (menu bar, buttons etc.) of the html page.
- */
-'use strict';
+import "./css/style.css";
+// import { DiscordSDK, DiscordSDKMock } from '@discord/embedded-app-sdk';
 import { settings } from '@pixi/settings';
 import { SCALE_MODES } from '@pixi/constants';
 import { Renderer, BatchRenderer, autoDetectRenderer } from '@pixi/core';
@@ -40,9 +12,9 @@ import { CanvasRenderer } from '@pixi/canvas-renderer';
 import { CanvasSpriteRenderer } from '@pixi/canvas-sprite';
 import { CanvasPrepare } from '@pixi/canvas-prepare';
 import '@pixi/canvas-display';
-import { PikachuVolleyball } from './pikavolley.js';
-import { ASSETS_PATH } from './assets_path.js';
-import { setUpUI } from './ui.js';
+import { PikachuVolleyball } from './js/pikavolley.js';
+import { ASSETS_PATH } from './js/assets_path.js';
+import { setUpUI } from './js/ui.js';
 
 // Reference for how to use Renderer.registerPlugin:
 // https://github.com/pixijs/pixijs/blob/af3c0c6bb15aeb1049178c972e4a14bb4cabfce4/bundles/pixi.js/src/index.ts#L27-L34
@@ -147,11 +119,7 @@ function setup() {
   start(pikaVolley);
 }
 
-/**
- * Start the game.
- * @param {PikachuVolleyball} pikaVolley
- */
-function start(pikaVolley) {
+function start(pikaVolley: PikachuVolleyball) {
   ticker.maxFPS = pikaVolley.normalFPS;
   ticker.add(() => {
     pikaVolley.gameLoop();
